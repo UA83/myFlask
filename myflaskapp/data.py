abca8aa472c1ae245faf3c57c8cba00675ee020a
Ulisses Alves <alves.enders@gmail.com>
2019-09-26 23:43:16 +0100

    LEarning about Flask

---
+++ myflaskapp/data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article one',
+            'body': '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ut, omnis minima vel maxime commodi ipsam? Rerum aliquid voluptates ut perspiciatis, magni iste eaque unde dicta cum similique laborum at.',
+            'autor' : 'Suki Dog',
+            'create_date': '26-09-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Dois',
+            'body': '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ut, omnis minima vel maxime commodi ipsam? Rerum aliquid voluptates ut perspiciatis, magni iste eaque unde dicta cum similique laborum at.',
+            'autor' : 'Ulisses Alves',
+            'create_date': '26-09-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article tres',
+            'body': '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ut, omnis minima vel maxime commodi ipsam? Rerum aliquid voluptates ut perspiciatis, magni iste eaque unde dicta cum similique laborum at.',
+            'autor' : 'Stephanie Endes',
+            'create_date': '26-09-2019'
+        }
+    ]
+
+    return articles
